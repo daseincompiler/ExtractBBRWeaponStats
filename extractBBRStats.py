91e85a913866c2166e01f2388b965b18b0e0033e
abbey <61197745+sneezingabbey@users.noreply.github.com>
2023-08-14 04:09:58 +0200

    added weapon categories

--- extractBBRStats.py
+++ extractBBRStats.py
@@ -5,15 +5,27 @@ import time
 from tkinter import Tk, filedialog
 
 categories = {
-    "Assault Rifles": ["AK47", "M4A1", "AK15", "SCARH", "AUG_A3", "SG550", "FAMAS", "ACR", "G36C", "HK419", "FAL", "AK5C"],
+    "Assault Rifles": ["AK74", "M4A1", "AK15", "SCAR-H", "AUG_A3", "SG550", "FAMAS", "ACR", "G36C", "HK419", "FAL", "AK5C"],
 
-    "Submachine Guns": ["MP7", "UMP45", "PP2000", "PP19", "KrissVector", "MP5"],
+    "Submachine Guns": ["MP7", "UMP-45", "PP2000", "PP19", "Kriss_Vector", "MP5"],
 
-    "Personal Defense Weapons": ["HoneyBadger", "Groza", "P90"],
+    "Personal Defense Weapons": ["Honey_Badger", "Groza", "P90"],
 
-    "Carbines": ["AS_VAL", "ScorpionEvo"],
+    "Carbines": ["As_Val", "ScorpionEVO"],
 
-    "Light Machine Guns": ["L86A1", "M249", "MG36"]
+    "Light Support Guns": ["L86A1", "MG36"],
+
+    "Light Machine Guns": ["M249", "Ultimax100"],
+
+    "Designated marksman Rifles": ["MK20", "M110", "MK14_EBR", "SVD"],
+
+    "Sniper Rifles": ["SSG_69", "SV-98", "L96", "Rem700", "M200", "MSR"],
+
+    "Pistols": ["M9", "MP_443", "USP"],
+
+    "Automatic Pistols ": ["Glock_18"],
+
+    "Heavy Caliber Pistols ": ["Unica", "Desert_Eagle", "Rsh12"]
 }
 
 
@@ -68,11 +80,26 @@ def main():
 
     weapon_count = len(weapons_data)
 
+    categorized_weapons = {}
+    for category, weapons in categories.items():
+        categorized_weapons[category] = {}
+        for weapon in weapons:
+            if weapon in weapons_data:
+                categorized_weapons[category][weapon] = weapons_data[weapon]
+            else:
+                print(
+                    f"Weapon '{weapon}' from category '{category}' not found in weapons_data!")
+
+    categorized_weapon_count = sum(len(weapons)
+                                   for weapons in categorized_weapons.values())
+
     output_data = {
         'update_version': "2.0.2",
         'date': datetime.datetime.now().strftime('%Y-%m-%d'),
         'weapon_count': weapon_count,
-        'weapons': weapons_data
+        'weapon_categories' : len(categorized_weapons),
+        'categorized_weapon_count': categorized_weapon_count,
+        'weapons': categorized_weapons
     }
 
     script_directory = os.path.dirname(os.path.abspath(__file__))
