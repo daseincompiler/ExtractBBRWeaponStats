6c06eed91ea4a80b670676fe7806ecbd1017baf5
abbey <61197745+sneezingabbey@users.noreply.github.com>
2023-08-14 02:44:12 +0200

    code improvements and more details into json

--- extractBBRStats.py
+++ extractBBRStats.py
@@ -1,12 +1,10 @@
 import os
 import json
+import datetime
+import time
 from tkinter import Tk, filedialog
 
-weapon_count = 0
-
 def extract_asset_data(directory_path):
-    global weapon_count
-
     weapons_data = {}
     
     for filename in os.listdir(directory_path):
@@ -26,37 +24,47 @@ def extract_asset_data(directory_path):
             for key in data_keys:
                 for line in content:
                     if key + ": " in line:
-                        weapon_data[key] = line.split(": ")[1].strip()
+                        # Convert all attributes to integers
+                        value = line.split(": ")[1].strip()
+                        weapon_data[key] = int(float(value))
                         break
 
             # Check if any key is missing from the extracted weapon data
             if all(key in weapon_data for key in data_keys):
                 weapons_data[weapon_name] = weapon_data
                 print(weapon_name)
-                weapon_count += 1
                 
     return weapons_data
 
 
 def main():
     Tk().withdraw()  # Hide the root tkinter window
+    print("Please select the MonoBehaviour Asset Folder, that was dumped by AssetRipper")
     directory_path = filedialog.askdirectory(title="Select the MonoBehaviour Asset Folder")
     
     if not directory_path:
         print("No directory selected. Exiting.")
+        time.sleep(3)
         return
 
     weapons_data = extract_asset_data(directory_path)
+
+    weapon_count = len(weapons_data)
+
+    output_data = {
+        'update_version' : "2.0.2",
+        'date': datetime.datetime.now().strftime('%Y-%m-%d'),
+        'weapon_count': weapon_count,
+        'weapons': weapons_data
+    }
     
     script_directory = os.path.dirname(os.path.abspath(__file__))
     output_filename = "weapons_data.json"
-    
-    with open(os.path.join(script_directory, output_filename), 'w') as json_file:
-        json.dump(weapons_data, json_file, indent=4)
+    with open(os.path.join(script_directory, output_filename), 'w') as json_file: json.dump(output_data, json_file, indent=4)
 
     print(f"{weapon_count} weapons had their data extracted!")
 
-    print(f"Extraction complete!")
+    print("Extraction complete!")
     input("Press Enter to exit...")
 
 if __name__ == "__main__":
