da4c3a89a27c44be88283c85b509f0a47f478216
abbey <61197745+sneezingabbey@users.noreply.github.com>
2023-08-14 02:17:20 +0200

    Update + data json dump
    upgraded script with more updated weapon stats + weapon_data.json weapon data dump

--- extractBBRStats.py
+++ extractBBRStats.py
@@ -1,50 +1,63 @@
 import os
 import json
+from tkinter import Tk, filedialog
 
-# Path to the directory containing the asset files
-directory_path = r"C:\Users\shant\Downloads\AssetRipper_win_x64\export\BattleBit\ExportedProject\Assets\MonoBehaviour"  # Modify this path to your directory
+weapon_count = 0
 
-# Dictionary to store extracted data
-weapons_data = {}
+def extract_asset_data(directory_path):
+    global weapon_count
 
-# Iterate over all files in the specified directory
-for filename in os.listdir(directory_path):
-    # Check if file contains an underscore and ends with '.asset'
-    if "_" in filename and filename.endswith(".asset"):
-        with open(os.path.join(directory_path, filename), 'r') as file:
-            content = file.readlines()
-        
-        # Extract weapon or gadget name by removing the index and underscore
-        weapon_name = filename.split("_", 1)[1].replace(".asset", "")
-        
-        vertical_mouse = None
-        horizontal_mouse = None
-        rounds_per_minute = None
-        
-        # Iterate through lines to find the required values
-        for line in content:
-            if "VerticalMouse:" in line:
-                vertical_mouse = line.split(":")[1].strip()
-            if "HorizontalMouse:" in line:
-                horizontal_mouse = line.split(":")[1].strip()
-            if "RoundsPerMinute:" in line:
-                rounds_per_minute = line.split(":")[1].strip()
-        
-        # Add extracted data to the dictionary only if none of the values are None
-        if all([vertical_mouse, horizontal_mouse, rounds_per_minute]):
-            weapons_data[weapon_name] = {
-                "VerticalMouse": vertical_mouse,
-                "HorizontalMouse": horizontal_mouse,
-                "RoundsPerMinute": rounds_per_minute
-            }
+    weapons_data = {}
+    
+    for filename in os.listdir(directory_path):
+        if "_" in filename and filename.endswith(".asset"):
+            with open(os.path.join(directory_path, filename), 'r') as file:
+                content = file.readlines()
+            
+            weapon_name = filename.split("_", 1)[1].replace(".asset", "")
+            data_keys = [
+                "DamageOnInfantryBody", "DamageOnLightArmoredVehicles", "DamageOnHeavyHeavyVehicles",
+                "BulletVelocity", "ProjectileVisualSize", "BaseHipFireAccuracy", "VerticalMouse",
+                "HorizontalMouse", "FirstShotRecoil", "AdsTime", "PlayerRunningSpeed",
+                "ReloadSpeed", "DrawSpeed", "RoundsPerMinute", "ShotSoundDistance"
+            ]
+            
+            weapon_data = {}
+            for key in data_keys:
+                for line in content:
+                    if key + ": " in line:
+                        weapon_data[key] = line.split(": ")[1].strip()
+                        break
 
-# Get the directory of the current script
-script_directory = os.path.dirname(os.path.abspath(__file__))
+            # Check if any key is missing from the extracted weapon data
+            if all(key in weapon_data for key in data_keys):
+                weapons_data[weapon_name] = weapon_data
+                print(weapon_name)
+                weapon_count += 1
+                
+    return weapons_data
 
-# Write the dictionary to a JSON file in the same directory as the script
-output_filename = "weapons_data.json"
-with open(os.path.join(script_directory, output_filename), 'w') as json_file:
-    json.dump(weapons_data, json_file, indent=4)
 
-print("Extraction complete!")
-input("Press Enter to exit...")
+def main():
+    Tk().withdraw()  # Hide the root tkinter window
+    directory_path = filedialog.askdirectory(title="Select the MonoBehaviour Asset Folder")
+    
+    if not directory_path:
+        print("No directory selected. Exiting.")
+        return
+
+    weapons_data = extract_asset_data(directory_path)
+    
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    output_filename = "weapons_data.json"
+    
+    with open(os.path.join(script_directory, output_filename), 'w') as json_file:
+        json.dump(weapons_data, json_file, indent=4)
+
+    print(f"{weapon_count} weapons had their data extracted!")
+
+    print(f"Extraction complete!")
+    input("Press Enter to exit...")
+
+if __name__ == "__main__":
+    main()
