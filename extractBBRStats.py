dc42846e5e8d2e4ba21d5e5c27d6c78401c18cf7
abbey <61197745+sneezingabbey@users.noreply.github.com>
2023-08-14 03:16:13 +0200

    added a part of a future categorization of weapons

--- extractBBRStats.py
+++ extractBBRStats.py
@@ -4,14 +4,27 @@ import datetime
 import time
 from tkinter import Tk, filedialog
 
+categories = {
+    "Assault Rifles": ["AK47", "M4A1", "AK15", "SCARH", "AUG_A3", "SG550", "FAMAS", "ACR", "G36C", "HK419", "FAL", "AK5C"],
+
+    "Submachine Guns": ["MP7", "UMP45", "PP2000", "PP19", "KrissVector", "MP5"],
+
+    "Personal Defense Weapons": ["HoneyBadger", "Groza", "P90"],
+
+    "Carbines": ["AS_VAL", "ScorpionEvo"],
+
+    "Light Machine Guns": ["L86A1", "M249", "MG36"]
+}
+
+
 def extract_asset_data(directory_path):
     weapons_data = {}
-    
+
     for filename in os.listdir(directory_path):
         if "_" in filename and filename.endswith(".asset"):
             with open(os.path.join(directory_path, filename), 'r') as file:
                 content = file.readlines()
-            
+
             weapon_name = filename.split("_", 1)[1].replace(".asset", "")
             data_keys = [
                 "DamageOnInfantryBody", "DamageOnLightArmoredVehicles", "DamageOnHeavyHeavyVehicles",
@@ -19,7 +32,7 @@ def extract_asset_data(directory_path):
                 "HorizontalMouse", "FirstShotRecoil", "AdsTime", "PlayerRunningSpeed",
                 "ReloadSpeed", "DrawSpeed", "RoundsPerMinute", "ShotSoundDistance"
             ]
-            
+
             weapon_data = {}
             for key in data_keys:
                 for line in content:
@@ -36,15 +49,16 @@ def extract_asset_data(directory_path):
             if all(key in weapon_data for key in data_keys):
                 weapons_data[weapon_name] = weapon_data
                 print(weapon_name)
-                
+
     return weapons_data
 
 
 def main():
     Tk().withdraw()  # Hide the root tkinter window
     print("Please select the MonoBehaviour Asset Folder, that was dumped by AssetRipper")
-    directory_path = filedialog.askdirectory(title="Select the MonoBehaviour Asset Folder")
-    
+    directory_path = filedialog.askdirectory(
+        title="Select the MonoBehaviour Asset Folder")
+
     if not directory_path:
         print("No directory selected. Exiting.")
         time.sleep(3)
@@ -55,20 +69,22 @@ def main():
     weapon_count = len(weapons_data)
 
     output_data = {
-        'update_version' : "2.0.2",
+        'update_version': "2.0.2",
         'date': datetime.datetime.now().strftime('%Y-%m-%d'),
         'weapon_count': weapon_count,
         'weapons': weapons_data
     }
-    
+
     script_directory = os.path.dirname(os.path.abspath(__file__))
     output_filename = "weapons_data.json"
-    with open(os.path.join(script_directory, output_filename), 'w') as json_file: json.dump(output_data, json_file, indent=4)
+    with open(os.path.join(script_directory, output_filename), 'w') as json_file:
+        json.dump(output_data, json_file, indent=4)
 
     print(f"{weapon_count} weapons had their data extracted!")
 
     print("Extraction complete!")
     input("Press Enter to exit...")
+
 
 if __name__ == "__main__":
     main()
